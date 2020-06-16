56f419a344e1e5fd5fc2ed8fde0103773dc089f2
Gussa Haile <haigussa@gmail.com>
2020-06-16 19:26:18 +0200

    Fix wrong Hour indicator hand

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const secondIndicator = document.getElementById('second-indicator')
 const minuteIndicator = document.getElementById('minute-indicator')
 const hourIndicator = document.getElementById('hour-indicator')
 
-    setInterval(() => {
-        const now = new Date()
-        // convert time units into degrees
-        const secondsDeg = now.getSeconds() * 6;
-        const minutesDeg = now.getMinutes() * 6
-        const hoursDeg = now.getHours() * 15;
-        secondIndicator.style.transform = `rotate(${secondsDeg + 90}deg)`
-        minuteIndicator.style.transform = `rotate(${minutesDeg + 90}deg)`
-        hourIndicator.style.transform = `rotate(${hoursDeg + 90}deg)`
-    }, 1000)
+setInterval(() => {
+    const now = new Date()
+    // convert time units into degrees
+
+    // 60 seconds * 6 = 360 deg => full circle
+    const secondsDeg = now.getSeconds() * 6;
+    
+    // 12 hours * 30 = 360 deg => full circle
+    const minutesDeg = now.getMinutes() * 6
+    
+    // 60 minutes * 6 = 360 deg => full circle
+    const hoursDeg = now.getHours() * 30;
+    secondIndicator.style.transform = `rotate(${secondsDeg}deg)`
+    minuteIndicator.style.transform = `rotate(${minutesDeg}deg)`
+    hourIndicator.style.transform = `rotate(${hoursDeg}deg)`
+}, 1000)
